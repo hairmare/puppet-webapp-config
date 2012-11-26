315877b043a7ad64e026a9aee5d73ceb4336cf6d
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-26 22:25:56 +0100

    style refactored to my likes and added rdoc docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,48 @@
+# == Define: webapp_config
+#
+# call webapp_config
+#
+# === Parameters:
+# [*action*]
+#   install or remove an app, default is install
+# [*vhost*]
+#   in what vhost to install
+# [*base*]
+#   what location to use
+# [*app*]
+#   what app to install, dont forget to install the Package elsewhere
+# [*version*]
+#   what version of the app to install
+# [*depends*]
+#   dependencies, like the Package and or File resources
+# [*installdir*]
+#   optional installdir location
+#
+define webapp_config ($action = 'install', $vhost, $base = '/', $app, $version, $depends, $installdir = undef) {
+  $installdir_real = $installdir ? {
+    undef   => "/var/www/${vhost}",
+    default => $installdir,
+  }
 
-define webapp_config ($action='install', $vhost, $base='/', $app, $version, $depends, $installdir=undef) {
+  case $action {
+    'install' : {
+      $cmd    = "webapp-config -I -h ${vhost} -d ${base} ${app} ${version}"
+      $unless = "test -f ${installdir_real}/htdocs/${base}/.webapp-${app}-${version}"
+    }
+    'remove'  : {
+      $cmd    = "webapp-config -C -h ${vhost} -d ${base} ${app} ${version}"
+      $unless = "$(! test -f ${installdir_real}/htdocs/${base}/.webapp-${app}-${version})"
+    }
+    default   : {
+      alert("webapp-config: Action '${action}' is invalid")
+    }
+  }
 
-    $_installdir = $installdir ? {
-        undef   => "/var/www/${vhost}",
-        default => $installdir,
-    }
-        
-    case $action {
-        "install":  { $cmd    = "webapp-config -I -h $vhost -d $base $app $version"
-                      $unless = "test -f ${_installdir}/htdocs/$base/.webapp-$app-$version"
-                    }
-        "remove" :  { $cmd    = "webapp-config -C -h $vhost -d $base $app $version"
-                      $unless = "$(! test -f ${_installdir}/htdocs/$base/.webapp-$app-$version)"
-                    }
-        default:    { alert( "webapp-config: Action '$action' is invalid" ) } 
-    }
-       
-    exec { "webapp-${action}-${app}":
-        command => $cmd,
-        path    => [ "/usr/bin", "/usr/sbin" ],
-        unless  => $unless,
-        require => $depends,
-    }
+  exec { "webapp-${action}-${app}":
+    command => $cmd,
+    path    => ['/usr/bin', '/usr/sbin'],
+    unless  => $unless,
+    require => $depends,
+  }
 }
-                        
+  
